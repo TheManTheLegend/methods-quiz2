71402db209c7b94979ba16d1ad1c0da0c44455cd
Nik Young <youngnr@s.dcsdk12.org>
2015-09-14 09:37:09 -0600

    methods complete

--- methods_quiz2.rb
+++ methods_quiz2.rb
@@ -1,5 +1,63 @@
-module MethodsQuiz
+module MethodsQuiz2
 	
+	def has_doubles?(a,b)
+		if a == b 
+			if a == 6 
+				a = 1
+			else
+				a = a + 1
+			end
+		end
+		return a+b
+	end
+
+	def max_maybe(a,b)
+		if a == b
+			return 0
+		elsif a % 5 == b % 5
+			if a < b
+				return a
+			else 
+				return b
+			end
+		else 
+			if a < b
+				return b
+			else
+				return a
+			end
+		end
+	end
+
+	def squirrels_play?(a,b)
+		if b == true
+			if a <= 100 && a >= 60
+				return true
+			else
+				return false
+			end
+
+		elsif b == false
+			if a <= 90 && a >= 60
+				return true
+			else
+				return false
+			end
+		end
+	end
 	
+	def red_ticket(a,b,c)
+		if a == 2 && b==2 && c==2
+			return 10
+		elsif a == b && a == c && a != 2
+			return 5
+		elsif a != b && a != c
+			return 1
+		else
+			return 0
+		end
+			
+		
+	end
 
 end
--- methods_quiz2_test.rb
+++ methods_quiz2_test.rb
@@ -12,5 +12,31 @@ class MethodsQuizTest < MiniTest::Test
 	end
 
 	# TODO - write tests here
+	def test_without_doubles
+		assert_equal 11, @m.has_doubles?(6,5)
+		assert_equal 7, @m.has_doubles?(6,6)
+		assert_equal 7, @m.has_doubles?(3,3)
+		assert_equal 3, @m.has_doubles?(2,1)
+	end
 
+	def test_max_maybe
+		assert_equal 0, @m.max_maybe(9,9)
+		assert_equal 1, @m.max_maybe(1,6)
+		assert_equal 3, @m.max_maybe(2,3)
+		assert_equal 7, @m.max_maybe(5,7)
+	end
+
+	def test_squirrels_play
+		assert_equal false, @m.squirrels_play?(95,false)
+		assert_equal true, @m.squirrels_play?(95,true)
+		assert_equal true, @m.squirrels_play?(60,true)
+		assert_equal true, @m.squirrels_play?(60,false)
+	end
+
+	def test_red_ticket
+		assert_equal 10, @m.red_ticket(2,2,2)
+		assert_equal 5, @m.red_ticket(1,1,1)
+		assert_equal 1, @m.red_ticket(0,1,2)
+		assert_equal 0, @m.red_ticket(1,1,2)
+	end
 end
